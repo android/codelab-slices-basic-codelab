78a3c2e58cc70ca8407f4e44bae3b368bca072d7
Jeremy Walker <codingjeremy@users.noreply.github.com>
2019-04-28 14:08:05 -0700

    Adds all required code for finished slice codelab.
    Change-Id: I8d8c92ff10389854f9c60ecf8f798b42699d3241

--- complete/src/main/java/com/example/android/slicesbasiccodelab/MainActivity.kt
+++ complete/src/main/java/com/example/android/slicesbasiccodelab/MainActivity.kt
@@ -13,15 +13,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.android.slicesbasiccodelab
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Displays the current temperature and allows user to adjust it up and down. Any adjustments from
+ * the external slice will also change this temperature value.
+ */
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var temperatureTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        temperatureTextView = findViewById(R.id.temperature)
+
+        findViewById<Button>(R.id.increase_temp).setOnClickListener(this)
+        findViewById<Button>(R.id.decrease_temp).setOnClickListener(this)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        temperatureTextView.text = getTemperatureString(applicationContext)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.increase_temp -> updateTemperature(applicationContext, temperature + 1)
+            R.id.decrease_temp -> updateTemperature(applicationContext, temperature - 1)
+        }
+        temperatureTextView.text = getTemperatureString(applicationContext)
+    }
+
+    companion object {
+
+        /* Temperature is in Celsius.
+         *
+         * NOTE: You should store your data in a more permanent way that doesn't disappear when the
+         * app is killed. This drastically simplified sample is focused on learning Slices.
+         */
+        private var temperature = 16
+
+        fun getTemperatureString(context: Context): String {
+            return context.getString(R.string.temperature, temperature)
+        }
+
+        fun getTemperature(): Int {
+            return temperature
+        }
+
+        fun updateTemperature(context: Context, newTemperature: Int) {
+
+            if (temperature != newTemperature) {
+                temperature = newTemperature
+
+                // Notify slice via URI that the temperature has changed so they can update views.
+                val uri = TemperatureSliceProvider.getUri(context, "temperature")
+                context.contentResolver.notifyChange(uri, null)
+            }
+        }
     }
 }
